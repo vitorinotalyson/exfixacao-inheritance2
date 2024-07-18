0e442cb1f774710459f742d69ada79eaecbf3e71
Talyson Vitorino <talysondev@gmail.com>
2024-07-18 13:38:28 -0300

    primeiro commit(adicionando classes)

---
+++ entities/Company.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Company extends TaxPayer {
+    private int numberOfEmployees;
+
+    public Company() {
+    }
+
+    public Company(String name, Double anualIncome, int numberOfEmployees) {
+        super(name, anualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public Double tax() {
+        return null;
+    }
+
+}
